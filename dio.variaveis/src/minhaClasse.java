f9a23353fb83dc5175a2b0208d9b4482ee576418
Razr <devpaulomaciel@gmail.com>
2024-06-09 10:03:59 -0300

    Teste de gravação de commits

--- dio.variaveis/src/minhaClasse.java
+++ dio.variaveis/src/minhaClasse.java
@@ -1,7 +1,7 @@
 public class minhaClasse {
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
     }
     
 }
